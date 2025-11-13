a032d799ff1972e57151da5b8e6b295592734fe9
1111-Elysia <1361109760@qq.com>
2025-11-13 18:32:48 +0800

    wbhs

--- src/serial_comm/src/serial_comm_node.cpp
+++ src/serial_comm/src/serial_comm_node.cpp
@@ -68,7 +68,7 @@ private:
     {
         std::lock_guard<std::mutex> lock(mutex_);
         
-        vx_   =  msg->linear.y / 10.0f;  
+        vx_   =  -msg->linear.y / 10.0f;  
         vy_   =  msg->linear.x / 10.0f;  
         vyaw_ =  msg->angular.z / 2.0f;
     }
