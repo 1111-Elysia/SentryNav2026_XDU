4557899805fe66ebd74c0e7acd3e243664acf016
1111-Elysia <1361109760@qq.com>
2026-01-19 16:19:58 +0800

    depend jiao ben

---
+++ depend_install.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install libopencv-dev libpcl-dev pcl-tools libyaml-cpp-dev libgoogle-glog-dev libgflags-dev ros-humble-pcl-conversions
+sudo apt install ros-humble-navigation2 ros-humble-nav2-msgs ros-humble-nav2-bringup ros-humble-pcl-ros ros-humble-tf2-ros
+sudo apt install ros-humble-rmw-cyclonedds-cpp
