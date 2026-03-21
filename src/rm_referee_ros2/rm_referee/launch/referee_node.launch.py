a78684edb025854156d7da6e8a032f02b23363d1
1111-Elysia <1361109760@qq.com>
2026-03-21 15:52:00 +0800

    wbhs

--- src/rm_referee_ros2/rm_referee/launch/referee_node.launch.py
+++ src/rm_referee_ros2/rm_referee/launch/referee_node.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
                 output='screen',
                 parameters=[{
                     "enable_normal": True,                 # 启用电源管理常规链路
-                    "enable_vt": True,                      # 启用图传链路
+                    "enable_vt": False,                      # 启用图传链路
                     "new_vt": True,                        # 如果是VT03新图传，把这一项设置为True，串口波特率会调整为921600，否则为115200
                     "normal_tty_device": "/dev/ttyACM0",    # 常规链路对应的串口设备
                     "vt_tty_device": "/dev/ttyACM0",        # 图传链路对应的串口设备
