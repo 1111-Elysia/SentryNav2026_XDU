802f3792db724921ee8cc469d9ff7d73272711b9
1111-Elysia <1361109760@qq.com>
2026-04-02 16:32:37 +0800

    依赖更新

--- depend_install.sh
+++ depend_install.sh
@@ -3,3 +3,4 @@ sudo apt install -y libopencv-dev libpcl-dev pcl-tools libyaml-cpp-dev libgoogle
 sudo apt install -y ros-humble-navigation2 ros-humble-nav2-msgs ros-humble-nav2-bringup ros-humble-pcl-ros ros-humble-tf2-ros ros-humble-robot-localization
 sudo apt install -y ros-humble-rmw-cyclonedds-cpp
 sudo apt install -y ros-humble-spatio-temporal-voxel-layer
+sudo apt-get install -y libboost-system-dev
